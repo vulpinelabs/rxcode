975dcfbfc911089c3ee6c2621580387c0e3cb541
Christian Niles <christian@nerdyc.com>
2011-05-19 11:07:40 -0700

    Moved project fixtures into 'projects' sub-directory

--- spec/support/fixtures.rb
+++ spec/support/fixtures.rb
@@ -2,10 +2,16 @@ module RXCode
   
   module Fixtures
     
-    PROJECT_FIXTURE_DIRECTORY = File.expand_path('../../fixtures', __FILE__)
+    # ===== PROJECTS ===================================================================================================
+    
+    PROJECT_FIXTURE_DIRECTORY = File.expand_path('../../fixtures/projects', __FILE__)
+    
+    def self.path_of_project(project_name)
+      File.join(PROJECT_FIXTURE_DIRECTORY, project_name, "#{project_name}.xcodeproj")
+    end
     
     def self.project_named(project_name)
-      fixture_path = File.join(PROJECT_FIXTURE_DIRECTORY, project_name, "#{project_name}.xcodeproj")
+      fixture_path = path_of_project(project_name)
       if RXCode::Project.is_project_at_path?(fixture_path)
         RXCode::Project.new(fixture_path)
       end
